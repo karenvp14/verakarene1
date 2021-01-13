0b43fced992df4bac174f48e275c63ef518bb24f
karenvp14 <74684065+karenvp14@users.noreply.github.com>
2021-01-12 19:43:46 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,53 +1,131 @@
 <?php
+// Initialize the session
+@session_start();
  
-include('config.php');
-session_start();
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+  header("location: inicio.html");
+  exit;
+}
  
-if (isset($_POST['login'])) {
+// Include config file
+require_once "config.php";
  
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
  
-    $query = $connection->prepare("SELECT * FROM login WHERE USERNAME=:username");
-    $query->bindParam("username", $username, PDO::PARAM_STR);
-    $query->execute();
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
  
-    $result = $query->fetch(PDO::FETCH_ASSOC);
- 
-    if (!$result) {
-        echo '<p class="error">Username password combination is wrong!</p>';
-    } else {
-        if (password_verify($password, $result['PASSWORD'])) {
-            $_SESSION['user_id'] = $result['ID'];
-            echo '<p class="success">Congratulations, you are logged in!</p>';
-        } else {
-            echo '<p class="error">Username password combination is wrong!</p>';
-        }
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Por favor ingrese su usuario.";
+    } else{
+        $username = trim($_POST["username"]);
     }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Por favor ingrese su contraseña.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            @session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: profesor.html");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "La contraseña que has ingresado no es válida.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "No existe cuenta registrada con ese nombre de usuario.";
+                }
+            } else{
+                echo "Algo salió mal, por favor vuelve a intentarlo.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
 }
- 
 ?>
-
-
-<form method="post" action="" name="signin-form">
-    <div class="form-element">
-        <label>Username</label>
-        <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
-    </div>
-    <div class="form-element">
-        <label>Password</label>
-        <input type="password" name="password" required />
-    </div>
-    <button type="submit" name="login" value="login">Log In</button>
-</form>
-
-<?php
-session_start();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <center>
  
-if(!isset($_SESSION['user_id'])){
-    header('Location: login.php');
-    exit;
-} else {
-    // Show login the page!
-}
-?>
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body background="https://fondosmil.com/fondo/1832.jpg">
+    <div class="wrapper">
+        <h2>INICIO DE SESION</h2>
+        <p>completer sus credenciales para iniciar sesión.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Usuario</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Contraseña</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                
+                 <input type="submit" class="btn btn-primary" value="Ingresar">
+                 </div>
+    
+            <p>¿No tienes una cuenta? <a href="registro_cliente.php">Regístrate ahora</a>.</p> 
+        </form>
+        
+ 
+</center>
+    </div>    
+</body>
+</html>
